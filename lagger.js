8a047b6e73d75330635e0818f685dfdf9ffbb117
AlbertLeipzig <albertclemvill@gmail.com>
2023-10-18 10:09:04 +0200

    added Musician class

---
+++ lagger.js
@@ -0,0 +1,31 @@
+// Higher Order Functions
+
+// map
+// forEach
+// filter
+// reduce
+// sort
+// find
+
+
+/* const filteredNumbers = numbers.filter(num => num > 2)
+
+console.log(filteredNumbers); */
+
+/* const doubleNumbers = numbers.map((number) => number * 2);
+
+console.log(doubleNumbers);
+
+const myArray = []
+
+numbers.forEach((number) => myArray.push(number * 2));
+
+console.log(myArray);
+
+const mitarbeiter = []
+
+mitarbeiter.forEach(_mitarbeiter => writeEmail(_mitarbeiter)) */
+
+
+
+
--- main.js
+++ main.js
@@ -1,3 +1,60 @@
-console.clear()
+console.clear();
 
-console.log(34563456);
+// Class Musician
+// Class Subscriber
+// Class Owner
+
+class Musician {
+  constructor(firstName, lastName, email, instruments) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.instruments = instruments;
+  }
+  printName (){
+    console.log(`${this.firstName} ${this.lastName}`);
+  }
+  get name(){
+    return `${this.firstName} ${this.lastName}`
+  }
+  printInstruments () {
+    this.instruments.forEach((instrument) => console.log(instrument));
+  }
+}
+
+
+
+const musician = {
+  firstName: '',
+  lastName: '',
+  email: '',
+  instruments: [],
+  printName: function () {
+    console.log(`${this.firstName} ${this.lastName}`);
+  },
+  printInstruments: function () {
+    this.instruments.forEach((instrument) => console.log(instrument));
+  },
+};
+
+const subscriber = {
+  firstName: '',
+  lastName: '',
+  email: '',
+  printName: function () {
+    console.log(`${this.firstName} ${this.lastName}`);
+  },
+};
+
+const owner = {
+  firstName: '',
+  lastName: '',
+  email: '',
+  business: [],
+  printName: function () {
+    console.log(`${this.firstName} ${this.lastName}`);
+  },
+  printBusiness: function () {
+    this.business.forEach((_business) => console.log(_business));
+  },
+};
